6e83d14bad96ad71be18230fa8a60c2ba6e84772
Kylie Baker <kyliebakeacake@gmail.com>
2013-03-25 19:31:05 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Village::Application.config.secret_token = '46d6d51cbcb6e09575357194f6d4c2c78c434758ab40cbc1bcb7ee8d8cf1c594f138c17eb0c8ce8dba57784a7526a3268d5ef0bcfaf09faee46f479f0ef08217'
